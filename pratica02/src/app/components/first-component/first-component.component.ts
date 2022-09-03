ab24adc6ca48fc67e179dbb522c12ecad629ff18
Tabata Souto <tabatac.souto@gmail.com>
2022-09-03 13:34:16 -0300

    feat: interpolação de dados

---
+++ pratica02/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  name: string = "Tabata";
+  age: number = 29;
+  job: string = "Programador";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
