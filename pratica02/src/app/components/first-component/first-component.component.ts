12bd6bb680175bba4ca88c4475663eafe48abc45
Tabata Souto <tabatac.souto@gmail.com>
2022-09-03 13:38:43 -0300

    feat: interpolação de dados

--- pratica02/src/app/components/first-component/first-component.component.ts
+++ pratica02/src/app/components/first-component/first-component.component.ts
@@ -9,6 +9,8 @@ export class FirstComponentComponent implements OnInit {
   name: string = "Tabata";
   age: number = 29;
   job: string = "Programador";
+  hobbies: string[] = ["Correr", "Jogar", "Estudar"];
+  car = { name: "Polo", year: 2019 };
 
   constructor() { }
 
